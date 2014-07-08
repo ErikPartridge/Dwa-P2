0094800b7bb491d3b09f4722fff0dd36cdc59dbd
Erik Malmstrom-Partridge <erazor101@gmail.com>
2014-07-08 14:10:59 -0400

    Made it look nice

--- index.php
+++ index.php
@@ -8,32 +8,42 @@
         ini_set('display_errors', 1);
         require 'Parser.php';
         ?>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <form action="#" method="GET">
-            Number of words:    <input name = "number" type="text" value="number" /><br>
-            First letter caps:  <input name = "capital" type="checkbox" value="capital"/><br>
-            Symbol in password: <input name = "symbol" type="checkbox" value="symbol" /><br>
-            Number in password: <input name = "num" type="checkbox" value="num" /><br>
-            <input type="submit" value="Submit"/>
-        </form>
-        <code><?PHP
-            if(isset($_GET["number"])){
-                $number = $_GET["number"];
-                $num = false;
-                if(isset($_GET["num"])){
-                    $num = $_GET["num"];
+        <div class = "block"/>
+            <p>
+                A different style password generator: as come up with by xkcd, this password generator uses a string of common words, which is more memorable than
+            </p>
+            <form action="#" method="GET">
+                Number of words:    <input name = "number" type="number" value="number" /><br>
+                First letter capitalized:  <input name = "capital" type="checkbox" value="capital"/><br>
+                Symbol in password: <input name = "symbol" type="checkbox" value="symbol" /><br>
+                Number in password: <input name = "num" type="checkbox" value="num" /><br>
+                <input type="submit" value="Submit"/>
+            </form>
+            <code><?PHP
+                if(isset($_GET["number"])){
+                    $number = $_GET["number"];
+                    $num = false;
+                    if(isset($_GET["num"])){
+                        $num = $_GET["num"];
+                    }
+                    $caps = false;
+                    if(isset($_GET["capital"])){
+                        $caps = $_GET["capital"];
+                    }
+                    $symbol = false;
+                    if(isset($_GET["symbol"])){
+                        $symbol = $_GET["symbol"];
+                    }
+                    print(getPassword($number, $num, $caps, $symbol));
                 }
-                $caps = false;
-                if(isset($_GET["capital"])){
-                    $caps = $_GET["capital"];
-                }
-                $symbol = false;
-                if(isset($_GET["symbol"])){
-                    $symbol = $_GET["symbol"];
-                }
-                print(getPassword($number, $num, $caps, $symbol));
-            }
-            ?></code>
+                ?></code>
+            <p>
+                ** This application uses an unedited list of common words in the English language. Expletive language is possible. **</br>
+            </p>
+            <img src = "http://imgs.xkcd.com/comics/password_strength.png" width = 95% height="95%"/>
+        </div>
     </body>
 </html>
